f6b1a836efa48ec9181135ce90db4a5600acf8b0
Manoj <manojmathe85@gmail.com>
2021-06-10 02:11:11 +0530

    Add files via upload

---
+++ src/app/button-cell-renderer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnDestroy } from '@angular/core';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
+import { ICellRendererParams } from 'ag-grid-community'; 
+import * as $ from 'jquery';
+
+@Component({
+    selector: 'btn-cell-renderer',
+    template: `
+    <button (click)="btnClickedHandler()">Click me!</button>
+    `,
+  })
+  export class BtnCellRenderer implements ICellRendererAngularComp, OnDestroy {
+    refresh(params: ICellRendererParams): boolean {
+        throw new Error('Method not implemented.');
+    }
+    private params: any;
+  
+    agInit(params: any): void {
+      this.params = params;
+    }
+  
+    btnClickedHandler() {
+      this.params.clicked(this.params.value);
+    }
+  
+    ngOnDestroy() {
+      // no need to remove the button click handler 
+      // https://stackoverflow.com/questions/49083993/does-angular-automatically-remove-template-event-listeners
+    }
+  }
